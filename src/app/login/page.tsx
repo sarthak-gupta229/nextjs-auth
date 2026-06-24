2f738f47ded86bc2740c9f6ea4f262a6313744f7
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-06-24 21:45:45 +0530

    next.js setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+  return (
+    <div className="flex ">
+        <h1 className="text-4xl justify-center text-white">
+            Login
+        </h1>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+    return(
+        <div>
+            <h1 className="text-4xl justify-center text-white">
+                Signup
+                </h1>
+        </div>
+    )
+}
