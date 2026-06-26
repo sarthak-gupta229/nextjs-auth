44914efe3d51d98d954f00bb3de5e26fe7fa519b
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-06-27 00:37:18 +0530

    feat:login page functionality

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextResponse, NextRequest } from 'next/server';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: 'User does not exist' },
+        { status: 400 }
+      );
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: 'Invalid password' }, { status: 400 });
+    }
+    console.log(user);
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: '1d',
+    });
+
+    const response = NextResponse.json({
+      message: 'Login successful',
+      success: true,
+    });
+
+    response.cookies.set('token', token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
